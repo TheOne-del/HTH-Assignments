1d0b646161880c563c354f3c9d6dde8cb4257526
Mezeeh <mezeeh7@gmail.com>
2026-06-17 13:55:03 -0700

    Add Python profile variables lab

---
+++ profile.py
@@ -0,0 +1,18 @@
+full_name = "Mezeeh Dolakeh"
+
+nickname = "Young Mez"
+
+age = 16
+
+used_python_before = "true"
+
+hobbies = ["gaming", "sports", "hiking", "build lego sculptures"]
+
+favorites = {
+ "anime": "Tensura",
+ "game": "Warframe",
+ "drink": "Jarritos",
+ "food": "Fried rice"
+}
+
+print(full_name,nickname,age,used_python_before,hobbies,favorites)
